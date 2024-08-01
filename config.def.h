464bda1d7e8eb8dc1c857c55dcd27b98cefd2b36
John Carlo Roberto <10111643+Irizwaririz@users.noreply.github.com>
2024-08-01 22:09:33 +0800

    Finalize status bar

--- config.def.h
+++ config.def.h
@@ -65,16 +65,16 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function       format           argument */
-	{ cpu_perc,       " CPU %s%% | ",  NULL         },
+	{ wifi_essid,     " %s ",           "wlp2s0"     },
+	{ netspeed_rx,    "%sB/s ↓ ",      "wlp2s0"     },
+	{ netspeed_tx,    "%sB/s ↑ | ",    "wlp2s0"     },
+	{ cpu_perc,       "CPU %s%% | ",  NULL         },
 	{ ram_used,       "RAM %s/",       NULL         },
 	{ ram_total,      "%s | ",         NULL         },
 	{ disk_free,      "/ %s | ",       "/"          },
-    { run_command,    "♪ %s | ",        "amixer sget Master | tail -1 | awk '{print $5 }' | sed 's/[][]//g'" },
+    { run_command,    "♪ %s | ",       "amixer sget Master | tail -1 | awk '{print $5 }' | sed 's/[][]//g'" },
 	{ battery_perc,   "⚡︎ %s%%",       "BAT0"       },
 	{ battery_state,  "%s |",          "BAT0"       },
 	{ datetime,       " %s",           "%a %b %d %T"},
 };
 
-	// { wifi_essid,     "[WIFI %s - ",   "eth0"       },
-	// { netspeed_rx,    "%sB/s ↓ ",      "eth0"       },
-	// { netspeed_tx,    "%sB/s ↑] ",     "eth0"       },
